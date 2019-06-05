c3dc7bb3f18e77db62c7dd160972eb51bf6b0f7c
asiuzzaman006 <zaman1304006@gmail.com>
2019-06-05 21:06:54 +0600

    Queue Implementation for a coordinate with LinkList

--- queueImplementation.cpp
+++ queueImplementation.cpp
@@ -1,72 +1,85 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
+struct Point
+{
+    int first;
+    int second;
+};
 
 struct Node
 {
-    int data;
+    Point pt;
     Node *next;
 };
 
-Node * front=NULL;
-Node * rear=NULL;
+Node *front = NULL;
+Node *rear = NULL;
 
-void Push(int x){
-    Node *temp=new Node();
-    temp->data=x;
-    temp->next=NULL;
-    if(front==NULL and rear==NULL){
-        front=rear=temp;
-        return;
+void Push(Point p)
+{
+    Node *temp = new Node();
+    temp->pt.first = p.first;
+    temp->pt.second = p.second;
+    temp->next = NULL;
+    if (front == NULL and rear == NULL)
+    {
+        front = rear = temp;
+        return; // ei return na dile choda khaba........
     }
-    rear->next=temp;
-    rear=temp; // eita na dile khobor asse......... memory overflow hobe mathai rakhis....
+    rear->next = temp;
+    rear = temp;
 }
 
-void Pop(){
-    Node *temp=new Node();
-    temp=front;
+void Pop()
+{
+    Node *temp = new Node();
+    temp = front;
     if(front==NULL) {
-        cout<<"Queue is empty.."<<endl;
-        return;
+        cout<<"The queue is Empty()...."<<endl;
     }
-    
-    if(front==rear){   /// Just contain one element 
-        front=rear=NULL;
-    } 
-    else {
-        front=front->next;
+    if (front == rear)
+    {
+        front = rear = NULL;
     }
-
-
+    else
+    {
+        front = front->next;
+    }
     free(temp);
 }
-
-int Front(){
+void Print()
+{
+    Node *temp = new Node();
+    temp = front;
+    while (temp != NULL)
+    {
+        cout << temp->pt.first << " " << temp->pt.second << endl;
+        temp = temp->next;
+    }
+    cout << endl;
+}
+Point Top(){
     
-    if(front==NULL) {
-        cout<<"Queue is Empty..."<<endl;
-    }
-    return front->data;
-}
-void Print(){
-    Node *temp=new Node();
-    temp=front;
-    while(temp!=NULL){
-        cout<<temp->data<<" ";
-        temp=temp->next;
-    }
-    cout<<endl;
+    return front->pt;
 }
 
-int main(){
-    cout<<"my main starts....."<<endl;
-  //  Print();
-    Push(4);
-    Push(44);
-    Print();
-    Push(11);
+int main()
+{
+    cout << "my main starts....." << endl;
+    Push({1, 10});
+    Push({2, 20});
+    //Print();
+    Push({3,30});
+    Push({4,40});
     Print();
     Pop();
     Print();
-    cout<<Front()<<endl;
+    cout<<"The Top: "<<Top().first<<" "<<Top().second<<endl;
+    Print();
+    Pop();
+    Print();
+    Push({6,60});
+    Print();
+
 }
