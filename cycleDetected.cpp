08a0042c8be680cc333f38eac801ad756f76909f
asiuzzaman006 <zaman1304006@gmail.com>
2019-06-13 23:54:38 +0600

    Cycle Detected using simple DFS showing the nodes

--- cycleDetected.cpp
+++ cycleDetected.cpp
@@ -1,59 +1,58 @@
 #include <iostream>
 using namespace std;
-int Graph[55][55];
-bool visited[55];
-int N, E; // size of the matrix...
+int node, edge;
+int mat[500][500];
+bool vis[500];
 
-bool DFS(int x)
+bool DFS(int u)
 {
-    if (visited[x])
-    {
+    if (vis[u])
         return true;
-    }
-   // cout<<x<<endl;
-    visited[x] = 1;
-    for (int i = 1; i <= N; i++)
+
+    vis[u] = true;
+
+    for (int i = 1; i <= node; i++)
     {
-        if (Graph[x][i])
+        if (mat[u][i])
         {
             if (DFS(i))
+            {
+                cout << i << " ";
                 return true;
+            }
         }
     }
     return false;
 }
-
 int main()
 {
-
-    cin >> N >> E;
-    for (int i = 0; i < E; i++)
+    cin >> node >> edge;
+    for (int i = 0; i < edge; i++)
     {
         int x, y;
         cin >> x >> y;
-        Graph[x][y] = 1;
+        mat[x][y] = 1;
     }
-       for(int i=1;i<=N;i++) {
-        for (int k = 1; k <= N; k++)
-        {
-            visited[k] = 0;
+    for (int i = 1; i <= node; i++)
+    {    
+        for(int k=1;k<=node;k++){
+            vis[k]=0;
         }
         if (DFS(i))
-            cout << "Detected...." << endl;
+        {
+            cout << "\nDetected" << endl;
+        }
         else
-            cout << "No" << endl;
-       }
+            cout << "\nNot Detected" << endl;
+    }
 }
 /*
-
-7 7
-1 2
+7 6
+1 2 
 2 3
 3 4
 4 5
-5 1
+5 6
 6 7
-6 6
-
-
+    
  */
