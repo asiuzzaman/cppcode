af7588535da14d95cd9e89668ea821aef8d7aa61
asiuzzaman006 <zaman1304006@gmail.com>
2019-07-19 10:48:34 +0600

    Using onle Queue Simple

--- Dijkstra_ShortestPath.cpp
+++ Dijkstra_ShortestPath.cpp
@@ -6,41 +6,39 @@ using namespace std;
 vector<pair<int,int> >v[MX];
 bool vis[MX];
 int dis[MX];
-
+int node,edge;
 
 void Dijkstra(){
 
-    memset(vis,false,sizeof(vis));
-    dis[1]=0;
-    multiset<pair<int,int> > S; 
-    S.insert({0,1});  // Weight must be come first  because multiset return the min priority
+   for(int i=1;i<=node;i++) dis[i]=MX;
+   dis[1]=0;
+   queue<int>Q;
+   Q.push(1);
+   while(!Q.empty()){
+       int u=Q.front();
+       Q.pop();
+     //  cout<<"the U size :"<<v[u].size()<<endl;
+       for(int i=0;i<v[u].size();i++){
+           int child=v[u][i].first;
+          // cout<<"Child: "<<child<<endl;
+           if(dis[child]>dis[u]+v[u][i].second) {
+               dis[child]=dis[u]+v[u][i].second;
+               
+               Q.push(child);
+           }
+       }
+   }
 
-    while(!S.empty()){
-        pair<int,int>p=*S.begin();
-        S.erase(S.begin());
-        int x=p.second;
-        int wei=p.first;
-        
-         if(vis[x]) continue;
-         vis[x]=true;
-       
-        for(int i=0;i<v[x].size();i++){
-            int e=v[x][i].first;
-            int w=v[x][i].second;
-            
-            if(dis[x]+w<dis[e]){
-                dis[e]=dis[x]+w;
-              
-                S.insert({dis[e],e});
-            }
-        }
-    }
+   for(int i=2;i<=node;i++){
+       cout<<dis[i]<<" ";
+   }
+
 }
 
 int main() {
 
-int node;cin>>node;    
-int edge;cin>>edge;
+cin>>node;    
+cin>>edge;
 
 for(int i=0;i<edge;i++){
    int n1,w,n2;
@@ -48,25 +46,19 @@ for(int i=0;i<edge;i++){
    v[n1].push_back({n2,w});
 }
 
-for(int i=0;i<=node;i++) dis[i]=INT_MAX;
 Dijkstra();
-for(int i=2;i<=node;i++){
-    cout<<dis[i]<<" ";
-}
-cout<<endl;
+
 
 }
 
 /*
 
-7
-1 2 1
-1 4 2
-1 3 7
-2 3 3
-3 5 1
-3 4 5
-4 5 7
+5 5
+1 2 5
+1 3 2
+3 4 1
+1 4 6
+3 5 5
 
 
 */
